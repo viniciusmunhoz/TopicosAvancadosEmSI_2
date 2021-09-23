6a6a6777af097dba351b45a7184c3c737cdc5702
Munhoz Rodrigues <vinimunhoz46@gmail.com>
2021-09-23 16:21:04 -0300

    COMMIT

---
+++ SolutionBoletim/ProjAPIBoletim/Data/ProjAPIBoletimContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjAPIBoletim.Models;
+
+namespace ProjAPIBoletim.Data
+{
+    public class ProjAPIBoletimContext : DbContext
+    {
+        public ProjAPIBoletimContext (DbContextOptions<ProjAPIBoletimContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjAPIBoletim.Models.Aluno> Aluno { get; set; }
+
+        public DbSet<ProjAPIBoletim.Models.Curso> Curso { get; set; }
+
+        public DbSet<ProjAPIBoletim.Models.Disciplina> Disciplina { get; set; }
+    }
+}
---
+++ SolutionBoletim/ProjMvcBoletim/Data/ProjMvcBoletimContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ProjMvcBoletim.Models;
+
+namespace ProjMvcBoletim.Data
+{
+    public class ProjMvcBoletimContext : DbContext
+    {
+        public ProjMvcBoletimContext (DbContextOptions<ProjMvcBoletimContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProjMvcBoletim.Models.Aluno> Aluno { get; set; }
+
+        public DbSet<ProjMvcBoletim.Models.Curso> Curso { get; set; }
+
+        public DbSet<ProjMvcBoletim.Models.Disciplina> Disciplina { get; set; }
+    }
+}
